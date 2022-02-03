7edb91097e2dfda094eb16c545e2d7bb886979c2
Kavya hb <kavyahb21@gmail.com>
2022-02-03 16:34:58 +0530

    final submission

--- src/com/flight/Main.java
+++ src/com/flight/Main.java
@@ -7,9 +7,11 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Flight fli = new Flight("12345", "12EQ", 200, 201);
+        System.out.println(fli.getFlightDetails());
         Passenger pas = new Passenger("Rajajinagar", "Bangalore", "Karnataka",
                 "Kavya", "9767733663", "kavya21@gmail.com");
-
+        System.out.println(pas.getAddressDetails());
+        System.out.println(pas.getContactDetails());
         RegularTicket reg = new RegularTicket("12RQ", "Bangalore", "Singapore",
                 fli, "05/02/2021 13:00:00", "05/02/2021 17:00:00", pas, "12A",
                 18000, true, "No");
